8713238ba7288419c83c88b16f22cf612c1bffe3
Zack Anderson <zack.anderson2011@gmail.com>
2021-07-21 14:27:52 -0500

    deleted license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return license (https://img.shields.io/badge/license-${answers.license}-brightgreen);
 }
 
 // TODO: Create a function that returns the license link
